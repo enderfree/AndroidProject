dc1e4c2fbbf2860dbe9f77a9c78bba02bb7acce4
enderfree <samuelfauteux7@gmail.com>
2021-10-12 13:40:19 -0400

    Created the firebase reference
    I had forgotten about it...

--- app/src/main/java/team3/samuelandsebastian/androidproject/service/FirebaseDAO.java
+++ app/src/main/java/team3/samuelandsebastian/androidproject/service/FirebaseDAO.java
@@ -1,14 +1,24 @@
 package team3.samuelandsebastian.androidproject.service;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class FirebaseDAO {
     private static FirebaseAuth auth;
+    private static DatabaseReference databaseReference;
 
     public static FirebaseAuth getAuth(){
         if(auth == null){
             auth = FirebaseAuth.getInstance();
         }
         return auth;
+    }
+
+    public static DatabaseReference getDatabaseReference(){
+        if(databaseReference == null){
+            databaseReference = FirebaseDatabase.getInstance().getReference();
+        }
+        return databaseReference;
     }
 }
